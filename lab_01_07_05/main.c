72765db8b09e3ebeb7d1f2582999d4b1f11e0fc7
siv22u975 <siv22u975@student.bmstu.ru>
2023-02-20 17:45:23 +0300

    lab_01_07_05 was updated.

--- lab_01_07_05/main.c
+++ lab_01_07_05/main.c
@@ -5,14 +5,32 @@
 #define ERR_IO 1
 #define ERR_WRONG_INPUT 2
 
-int func_f(int x)
+double func_f(double x)
 {
     return 1/(sqrt(1-(x*x)));
 }
 
+double func_s(double x, double eps)
+{
+    double n = 1;
+    double x_n = 1;
+    double result = 1;
+
+    while (fabs(x_n) >= eps)
+    {
+        x_n *= n * x * x / (n + 1);
+        n += 2;
+        result += x_n;
+    }
+    return result;
+}
+
+
+
 int main(void)
 {
     double x, eps;
+    double func_real, func_eps, abs_err, rel_err;
     
     printf("Введите x и eps:\n");
     if (scanf("%lf%lf", &x, &eps) != 2)
@@ -26,6 +44,13 @@ int main(void)
         return ERR_WRONG_INPUT;
     }
 
-    
+    func_real = func_f(x);
+    func_eps = func_s(x, eps);
+    abs_err = fabs(func_real - func_eps);
+    rel_err = abs_err / fabs(func_real);
+
+    printf("%f\n%f\n%f\n%f\n", func_real, func_eps, abs_err, rel_err);
+
+    return OK;
 
 }
