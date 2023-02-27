6e10a5916e949f56e8bc91aed0aec06bcab2426f
Ivan <siv22u975@student.bmstu.ru>
2023-02-27 16:31:38 +0300

    build error fixed

--- lab_01_02_01/main.c
+++ lab_01_02_01/main.c
@@ -11,6 +11,8 @@ int main(void)
     
     printf("Введите основания равн/бедр. трапеции и угол (в градусах) при большем из них (a, b, phi):\n");
 
+    scanf("%lf%lf%lf", &a, &b, &phi);
+
     double s = fabs(a * a - b * b) / 2 * tan((phi * m_pi / 180));
 
     printf("S = %.6f\n", s);
