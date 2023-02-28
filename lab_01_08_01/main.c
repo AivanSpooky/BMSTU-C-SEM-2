71f5f57705fc499037b89a6d711235e58309bcee
Smirnov Ivan <siv22u975@student.bmstu.ru>
2023-02-28 10:42:03 +0300

    lab_01_08_01 added

--- lab_01_08_01/main.c
+++ lab_01_08_01/main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <limits.h>
 
 #define OK 0
 #define ERR_IO 1
+#define ERR_RANGE 2
 
-unsigned int pack(unsigned char b1, unsigned char b2, unsigned char b3, unsigned char b4)
+void bin_print(uint32_t u)
 {
-    unsigned int u = b1;
+	size_t nbits = sizeof(u) * CHAR_BIT;
+	uint32_t mask = 1 << (nbits - 1);
+	
+	for (size_t i = 0; i < nbits; i++)
+	{
+		if (u & mask)
+			printf("1");
+		else
+			printf("0");
+		mask >>= 1;
+	}
+	printf("\n");
+}
+
+uint32_t pack(uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4)
+{
+    uint32_t u = b1;
     u <<= 8;
     u |= b2;
     u <<= 8;
@@ -16,9 +35,22 @@ unsigned int pack(unsigned char b1, unsigned char b2, unsigned char b3, unsigned
     return u;
 }
 
+void unpack_print(uint32_t u)
+{
+	int bytes = 4;
+	for (int i = 0; i < bytes; i++)
+	{
+		uint32_t tmp = u;
+		tmp <<= i*8;
+		tmp >>= 24;
+		printf("%u", tmp);
+	}
+	printf("\n");
+}
+
 int main(void)
 {
-    unsigned char byte1, byte2, byte3, byte4;
+    uint8_t byte1, byte2, byte3, byte4;
 
     printf("Введите значения 4х байт (в 10-ой):\n");
     if (scanf("%hhu%hhu%hhu%hhu", &byte1, &byte2, &byte3, &byte4) != 4)
@@ -26,7 +58,9 @@ int main(void)
         return ERR_IO;
     }
 
-    printf("%u\n", pack(byte1, byte2, byte3, byte4));
+    uint32_t packed_int = pack(byte1, byte2, byte3, byte4);
+	bin_print(packed_int);
+	unpack_print(packed_int);
 
     return OK;
 }
