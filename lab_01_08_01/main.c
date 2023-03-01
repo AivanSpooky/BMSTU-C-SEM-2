f6bb50f969f70ca5c94949d79c1ff5ab2c00381b
Smirnov Ivan <1709-siv22u975@users.noreply.git.iu7.bmstu.ru>
2023-03-01 20:04:05 +0300

    Update main.c

--- lab_01_08_01/main.c
+++ lab_01_08_01/main.c
@@ -53,7 +53,7 @@ int main(void)
 	uint8_t byte1, byte2, byte3, byte4;
 
 	printf("Введите значения четырех байт (в 10-ой):\n");
-	if (scanf("%hhu%hhu%hhu%hhu", &byte1, &byte2, &byte3, &byte4) != 4)
+	if (scanf("%u%u%u%u", &byte1, &byte2, &byte3, &byte4) != 4)
 	{
 		printf("IO error!");
 		return ERR_IO;
