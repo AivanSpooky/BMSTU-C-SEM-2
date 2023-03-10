694ee4d29486f4c3063966c24524e6efbf454d5a
Ivan <siv22u975@student.bmstu.ru>
2023-03-10 18:11:37 +0300

    Tests passed percentage added

--- lab_01_05_05/func_tests/scripts/func_tests.sh
+++ lab_01_05_05/func_tests/scripts/func_tests.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 test_ok="0"
+files_count=0
 count_errors=0
 
 files="../data/pos_??_in.txt"
 for file_in in $files; do
+	if [ -f "$file_in" ]; then
+		number=$(echo "$file_in" | grep -o "[0-9]*")
+	else
+		echo "No positive tests"
+		continue
+	fi
 	number=$(echo "$file_in" | grep -o "[0-9]*")
 
 	file_out="../data/pos_""$number""_out.txt"
@@ -13,8 +20,9 @@ for file_in in $files; do
 		./pos_case.sh $file_in $file_out
         error="$?"
 	else
-		echo "pos_""$number""_in"": FAILED"
+		echo "pos_""$number""_in"": FAIL (No Output File)"
 		count_errors=$((count_errors + 1))
+		files_count=$((files_count + 1))
 		continue
 	fi
 
@@ -24,15 +32,19 @@ for file_in in $files; do
 		echo "pos_""$number""_in"": FAIL"
 		count_errors=$((count_errors + 1))
 	fi
+	files_count=$((files_count + 1))
 done
 
+echo
 files="../data/neg_??_in.txt"
 for file_in in $files; do
-	number=$(echo "$file_in" | grep -o "[0-9]*")
-	if [ -z "$number" ]; then
-		break
+	if [ -f "$file_in" ]; then
+		number=$(echo "$file_in" | grep -o "[0-9]*")
+	else
+		echo "No negative tests"
+		continue
 	fi
-
+	
     ./neg_case.sh $file_in
     error="$?"
 
@@ -42,6 +54,11 @@ for file_in in $files; do
 		echo "neg_""$number""_in"": FAIL"
 		count_errors=$((count_errors + 1))
 	fi
+	files_count=$((files_count + 1))
 done
 
+percentage=$(echo "scale=9; ($files_count-$count_errors)/$files_count*100" | bc )
+echo
+echo "Tests passed (in %): "
+echo $percentage | awk '{printf "%.0f\n",$1}'
 exit "$count_errors"
